77bab60950d8d34ebd601259bed1c97418caefa3
plopp00 <nihad.mujcic.te15c@gmail.com>
2024-10-04 00:04:38 +0200

    Bugfix: Update AVR_CPU_HZ to 2 MHz

--- ports/avr/atomport-private.h
+++ ports/avr/atomport-private.h
@@ -31,7 +31,7 @@
 #define __ATOM_PORT_PRIVATE_H
 
 /* CPU Frequency */
-#define AVR_CPU_HZ          1000000
+#define AVR_CPU_HZ          2000000
 
 /* Function prototypes */
 void avrInitSystemTickTimer ( void );
